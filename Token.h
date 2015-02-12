561a849d0a7ed46d810fed0c9c5950017f37b45f
danben4 <danielbenediktsson4@gmail.com>
2015-02-12 13:10:40 +0000

    Finished the Token.h file

---
+++ Token.h
@@ -0,0 +1,14 @@
+#define TOKEN_H
+#include <string>
+
+using namespace std;
+
+enum TokenCode {ID, ASSIGN, SEMICOL, INT, PLUS, MINUS, MULT, LPAREN, RPAREN, PRINT, END, ERROR};
+
+class Token {
+public:
+	Token(string lexeme, TokenCode tCode) : lexeme(lexeme), tCode(tCode) { };
+private:
+	string lexeme;
+	TokenCode tCode;
+};

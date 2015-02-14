6c53b447122541e2e8653f087ab8b4c510de93e5
danben4 <danielbenediktsson4@gmail.com>
2015-02-14 12:07:53 +0000

    finished

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -11,14 +11,19 @@ int main(){
 	string s;
 
 	while(cin >> s) {
-		cout << "inside while" << endl;
 		if (s == "PUSH") {
-			cout << "push" << endl;
-			cin >> s;
-			myIDs.push(s);
+			if(cin >> s) {
+				myIDs.push(s);
+			} else {
+				cout << "Error for operator: " << s << endl;
+				exit(0);
+			}
 		} else if (s == "ASSIGN") {
-			// second = first;
-			cout << "assign" << endl;
+			if(myIDs.size() < 2) {
+				cout << "Error for operator: " << s << endl;
+				exit(0);
+			}
+
 			string first, second;
 			int number;
 			first = myIDs.top();
@@ -26,48 +31,61 @@ int main(){
 			second = myIDs.top();
 			myIDs.pop();
 
-			if(isdigit(first[0])) {
-				number = stoi(first);
-			} else {
+			if(myMap.count(first) > 0) {
 				number = myMap.find(first)->second;
+			} else {
+				number = stoi(first);
 			}
+			
 			myMap.insert(pair<string, int>(second, number));
-		} else if (s == "SUB") {
-			/*
-
-
-
-
-			string first = myIDs.top();
-			myIDs.pop();
-			string second = myIDs.top();
-			myIDs.pop();
-			int firstnumber, secondnumber;
-			if(isdigit(first[0])) {
-				firstnumber = stoi(first);
-			} else {
-				firstnumber = myMap.find(first)->second;
-			}
-			if(isdigit(second[0])) {
-				secondnumber = stoi(first);
-			} else {
-				secondnumber = myMap.find(second)->second;
-			}
-			int result = secondnumber - firstnumber;
-			string myString = to_string(result);
-			myIDs.push(myString);
-			*/
-		} else if (s == "PLUS") {
-			
-		} else if (s == "MULT") {
-			
 		} else if (s == "PRINT") {
-			cout << "print" << endl;
+			if(myMap.empty()) {
+				cout << "Error for operator: " << s << endl;
+				exit(0);
+			}
 			cout << myMap.find(myIDs.top())->second << endl;
 			myIDs.pop();
+		} else if((s == "SUB") || (s == "ADD") || (s == "MULT")) {
+			if(myIDs.size() < 2) {
+				cout << "Error for operator: " << s << endl;
+				exit(0);
+			}
+
+			string first, second;
+			int firstnumber, secondnumber;
+			first = myIDs.top();
+			myIDs.pop();
+			second = myIDs.top();
+			myIDs.pop();
+
+			if(myMap.count(first) > 0) {
+				firstnumber = myMap.find(first)->second;
+			} else {
+				firstnumber = stoi(first);
+			}
+
+			if(myMap.count(second) > 0) {
+				secondnumber = myMap.find(second)->second;
+			} else {
+				secondnumber = stoi(second);
+			}
+
+			int result = 0;
+			if(s == "SUB") {
+				result = secondnumber - firstnumber;
+			} else if(s == "ADD") {
+				result = secondnumber + firstnumber;
+			}
+			else if(s == "MULT") {
+				result = secondnumber * firstnumber;
+			}
+
+			myIDs.push(to_string((long long) result));
+		} else {
+			cout << "Error for operator: " << s << endl;
+			exit(0);
 		}
 	}
-
 
 	return 0;
 }
